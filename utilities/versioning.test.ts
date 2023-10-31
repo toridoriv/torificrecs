c5e756c0102f8d279c7956a3b238f6c78e045d4a
toridoriv <vrodriguezfe@icloud.com>
2023-10-31 20:14:40 -0300

    :sparkles: Add versioning module

---
+++ utilities/versioning.test.ts
@@ -0,0 +1,69 @@
+import { config } from "@config";
+import { getCurrentVersion, SemVer, VersionObject } from "@utilities/versioning.ts";
+import { expect } from "chai";
+import { describe, it } from "std/testing/bdd.ts";
+
+describe("class VersionObject", () => {
+  it("should create a new VersionObject given a version string", () => {
+    const version = "1.0.0";
+    const versionObject = new VersionObject(version);
+
+    expect(versionObject).to.be.instanceof(VersionObject);
+    expect(versionObject).to.have.keys("version", "tag", "semver");
+  });
+
+  it("should create a new VersionObject given a semver object", () => {
+    const version: SemVer = {
+      major: 1,
+      minor: 0,
+      patch: 0,
+      prerelease: [],
+      build: [],
+    };
+    const versionObject = new VersionObject(version);
+
+    expect(versionObject).to.be.instanceof(VersionObject);
+    expect(versionObject).to.have.keys("version", "tag", "semver");
+  });
+
+  describe("its method", () => {
+    const version = "1.0.0";
+    const versionObject = new VersionObject(version);
+
+    it(".getNextPatch should return a version object for the next Patch version", () => {
+      const nextVersionObject = versionObject.getNextPatch();
+
+      expect(nextVersionObject.version).to.equal("1.0.1");
+    });
+
+    it(".getNextMinor should return a version object for the next Minor version", () => {
+      const nextVersionObject = versionObject.getNextMinor();
+
+      expect(nextVersionObject.version).to.equal("1.1.0");
+    });
+
+    it(".getNextMajor should return a version object for the next Major version", () => {
+      const nextVersionObject = versionObject.getNextMajor();
+
+      expect(nextVersionObject.version).to.equal("2.0.0");
+    });
+
+    it(".getNextVersion should return a version object for the next version given a release type", () => {
+      const patch = versionObject.getNextVersion("patch");
+      const minor = versionObject.getNextVersion("minor");
+      const major = versionObject.getNextVersion("major");
+
+      expect(patch.version).to.equal("1.0.1");
+      expect(minor.version).to.equal("1.1.0");
+      expect(major.version).to.equal("2.0.0");
+    });
+  });
+});
+
+describe("function getCurrentVersion", () => {
+  const currentVersion = getCurrentVersion();
+
+  it("should create a version object based on the current version of this project", () => {
+    expect(currentVersion.version).to.equal(config.project.version);
+  });
+});
---
+++ utilities/versioning.ts
@@ -0,0 +1,91 @@
+export type { SemVer } from "std/semver/mod.ts";
+
+import { config } from "@config";
+import { format, increment, parse, type SemVer } from "std/semver/mod.ts";
+
+const NEXT_VERSION_METHOD_BY_RELEASE_TYPE = {
+  patch: "getNextPatch",
+  minor: "getNextMinor",
+  major: "getNextMajor",
+} as const;
+
+/** The type of release to perform when generating the next version. */
+export type ReleaseType = keyof typeof NEXT_VERSION_METHOD_BY_RELEASE_TYPE;
+
+/**
+ * Represents a version object that encapsulates a semantic version and provides methods
+ * to obtain the next version based on different release types.
+ */
+export class VersionObject {
+  /**
+   * Get the method name to retrieve the next version based on the specified release type.
+   *
+   * @param type - The type of release (major, minor, or patch).
+   * @returns The name of the method for obtaining the next version.
+   */
+  static getMethodNameByReleaseType(type: ReleaseType) {
+    return NEXT_VERSION_METHOD_BY_RELEASE_TYPE[type];
+  }
+
+  readonly version: string;
+  readonly tag: string;
+  readonly semver: SemVer;
+
+  constructor(rawVersion: string | SemVer) {
+    if (typeof rawVersion === "string") {
+      this.version = rawVersion;
+      this.semver = parse(rawVersion);
+    } else {
+      this.semver = rawVersion;
+      this.version = format(this.semver);
+    }
+
+    this.tag = `v${this.version}`;
+  }
+
+  /**
+   * Get the next version by the specified release type.
+   *
+   * @param type - The type of release (major, minor, or patch).
+   * @returns A new `VersionObject` instance representing the next version.
+   */
+  getNextVersion(type: ReleaseType) {
+    return this[VersionObject.getMethodNameByReleaseType(type)]();
+  }
+
+  /**
+   * Get the next patch version.
+   *
+   * @returns A new VersionObject instance representing the next patch version.
+   */
+  getNextPatch() {
+    return new VersionObject(increment(this.semver, "patch"));
+  }
+
+  /**
+   * Get the next minor version.
+   *
+   * @returns A new VersionObject instance representing the next minor version.
+   */
+  getNextMinor() {
+    return new VersionObject(increment(this.semver, "minor"));
+  }
+
+  /**
+   * Get the next major version.
+   *
+   * @returns A new VersionObject instance representing the next major version.
+   */
+  getNextMajor() {
+    return new VersionObject(increment(this.semver, "major"));
+  }
+}
+
+/**
+ * Gets the current version number from the project configuration.
+ *
+ * @returns A VersionObject instance representing the current version.
+ */
+export function getCurrentVersion() {
+  return new VersionObject(config.project.version);
+}
